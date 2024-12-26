dffd4fce5022e5c5eea3b687442cd7b40ac58d96
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2024-12-26 10:37:50 -0400

    button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 
 import './App.css'
 import { Button } from './components/ui/Button'
+import { PlusIcon } from './icons/PlusIcon';
 
 function App() {
 
+  function buttonOnClick(){
+    console.log("button was clicked");
+    
+  }
+
   return (
     <>
-      {/* <Button/> */}
+      <Button  size="sm" variant='primary' text='Share' onClick={buttonOnClick}/>
+      <Button  size="md" variant='secondary' text='Add Content' startIcon={<PlusIcon/>}/>
+      <Button  size="lg" variant='secondary' text='Add Content'/>
     </>
   )
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,14 +1,29 @@
+import { ReactElement } from "react";
+
 interface ButtonProps {
     variant : "primary" | "secondary";
     size : "sm" | "md" | "lg";
     text : string;
-    startIcon? : any;
-    endIcon? : any;
+    startIcon? : ReactElement;
+    endIcon? : ReactElement;
     onClick : () => void;
 }
+// use maps and records here 
+
+const variantStyles = {
+    "primary" : "bg-purple-600 text-white",
+    "secondary" : "bg-purple-300 text-purple-600"
+}
+const sizeStyles = {
+    "sm" : "py-1 px-2",
+    "md" : "py-2 px-4",
+    "lg" : "py-4 px-6"  
+}
+
+const defaultStyles = "rounded-md flex"
 
 export const Button = (props : ButtonProps) => {
-    return <button></button>
+    return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}> {props.startIcon? <div className="pr-2">{props.startIcon}</div> : null} {props.text} {props.endIcon}</button>
 }
 
 <Button variant="primary" size="md" onClick={()=>{}} text={"asf"} />
